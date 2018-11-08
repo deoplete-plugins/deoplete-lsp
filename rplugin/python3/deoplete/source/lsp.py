2161d6fe1c50f97d92d3f5bf1e6e6385a0ed11ff
Luis Carlos Cruz Carballo <lcruzc@linkux-it.com>
2018-11-08 18:49:03 -0400

    Async lsp

--- rplugin/python3/deoplete/source/lsp.py
+++ rplugin/python3/deoplete/source/lsp.py
@@ -3,9 +3,28 @@
 # AUTHOR: Shougo Matsushita <Shougo.Matsu at gmail.com>
 # =============================================================================
 
-import re
-
 from .base import Base
+
+LSP_KINDS = [
+    'Text',
+    'Method',
+    'Function',
+    'Constructor',
+    'Field',
+    'Variable',
+    'Class',
+    'Interface',
+    'Module',
+    'Property',
+    'Unit',
+    'Value',
+    'Enum',
+    'Keyword',
+    'Snippet',
+    'Color',
+    'File',
+    'Reference'
+]
 
 
 class Source(Base):
@@ -17,6 +36,9 @@ class Source(Base):
         self.rank = 500
         self.input_pattern = r'\.[a-zA-Z0-9_?!]*|[a-zA-Z]\w*::\w*|->\w*'
         self.vars = {}
+        self.vim.vars['deoplete#source#lsp#_results'] = []
+        self.vim.vars['deoplete#source#lsp#_success'] = False
+        self.vim.vars['deoplete#source#lsp#_requested'] = False
 
     def gather_candidates(self, context):
         if not self.vim.call('exists', '*lsp#server#add'):
@@ -26,24 +48,45 @@ class Source(Base):
                              'require("lsp.plugin").client.has_started()'):
             return []
 
-        location = {
-            'position': {
-                'character': context['complete_position'],
-                'line': self.vim.call('line', '.') - 1,
+        if context['is_async']:
+            if self.vim.vars['deoplete#source#lsp#_requested']:
+                context['is_async'] = False
+                return self.process_candidates()
+            return []
+        else:
+            self.vim.vars['deoplete#source#lsp#_requested'] = False
+            context['is_async'] = True
+
+            location = {
+                # not working sending the buffer
+                # 'textDocument': ''.join(self.vim.current.buffer[:]),
+                'position': {
+                    'character': context['complete_position'],
+                    'line': self.vim.call('line', '.') - 1,
+                }
             }
-        }
 
-        # Todo: Async support
-        candidates = self.vim.call(
-            'lsp#request', 'textDocument/completion', location)
+            self.vim.call('luaeval',
+                          'require("deoplete").request_candidates(_A.arguments, _A.filetype)',
+                          {'arguments': location, 'filetype': context['filetype']})
 
-        for candidate in candidates:
-            word = candidate['word']
-            candidate['word'] = re.sub(r'\([^)]*\)', '', word)
-            candidate['abbr'] = word
-            if isinstance(candidate.get('info', None), str):
-                candidate['info'] = re.sub(r'\n.*', '', candidate['info'])
-            else:
-                candidate['info'] = ''
+            return []
 
-        return candidates
+    def process_candidates(self):
+        completions = []
+        for rec in self.vim.vars['deoplete#source#lsp#_results']['items']:
+            item = {
+                'dup': 0,
+            }
+            item['word'] = rec.get('entryName', rec.get('label'))
+            item['abbr'] = rec['label']
+
+            if 'kind' in rec:
+                item['kind'] = LSP_KINDS[rec['kind']]
+
+            if 'detail' in rec:
+                item['info'] = rec['detail']
+
+            completions.append(item)
+
+        return completions
