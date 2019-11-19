fa051438aae3a34976cd0cc9c19fe29545c8134e
erw7 <erw7.github@gmail.com>
2019-11-19 20:40:23 +0900

    Update for current lsp api

--- lua/deoplete.lua
+++ lua/deoplete.lua
@@ -4,16 +4,16 @@
 --------------------------------------------------------------------------------
 --
 
-local get_candidates = function(success, data)
-  vim.api.nvim_set_var('deoplete#source#lsp#_results', data)
+local get_candidates = function(_, _, result)
+  local success = (result and not vim.tbl_isempty(result)) and true or false
+  vim.api.nvim_set_var('deoplete#source#lsp#_results', result)
   vim.api.nvim_set_var('deoplete#source#lsp#_success', success)
   vim.api.nvim_set_var('deoplete#source#lsp#_requested', true)
   vim.api.nvim_call_function('deoplete#auto_complete', {})
 end
 
-local request_candidates = function(arguments, filetype)
-  vim.lsp.request_async('textDocument/completion',
-                               arguments, get_candidates, nil, filetype)
+local request_candidates = function(arguments)
+  vim.lsp.buf_request(0, 'textDocument/completion', arguments, get_candidates)
 end
 
 
