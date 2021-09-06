e3332efcbeecd016fe62ee74e3d8c1bd4b4cb5dc
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-06 19:33:25 +0900

    Fix for neovim 0.6 LSP

--- lua/candidates.lua
+++ lua/candidates.lua
@@ -6,12 +6,24 @@
 
 local api = vim.api
 
-local get_candidates = function(_, _, result)
-  local success = (type(result) == 'table' and not vim.tbl_isempty(result)) and true or false
-  api.nvim_set_var('deoplete#source#lsp#_results', result)
-  api.nvim_set_var('deoplete#source#lsp#_success', success)
-  api.nvim_set_var('deoplete#source#lsp#_requested', true)
-  api.nvim_call_function('deoplete#auto_complete', {})
+local get_candidates = function(_, arg1, arg2)
+  -- For neovim 0.6 breaking changes
+  -- https://github.com/neovim/neovim/pull/15504
+  local result = vim.fn.has('nvim-0.6') and arg1 or arg2
+  if not result or result == 0 then
+    return
+  end
+
+  local success = (type(result) == 'table' and not vim.tbl_isempty(result)
+    ) and true or false
+  result = result['items'] ~= nil and result['items'] or result
+
+  if #result > 0 then
+    api.nvim_set_var('deoplete#source#lsp#_results', result)
+    api.nvim_set_var('deoplete#source#lsp#_success', success)
+    api.nvim_set_var('deoplete#source#lsp#_requested', true)
+    api.nvim_call_function('deoplete#auto_complete', {})
+  end
 end
 
 local request_candidates = function(arguments)
